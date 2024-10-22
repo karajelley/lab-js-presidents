48e5b8361e531358bfc470bc6aa4374ed99d5ddb
Kara Jelley <karajelley@gmail.com>
2024-10-22 08:23:10 -0400

    Iteration 2 done

--- src/presidents.js
+++ src/presidents.js
@@ -419,7 +419,13 @@ const presidents = [
 
 
 // Iteration 1 | Names of All Presidents - `map()`
-function getNames(presidentsArr) {}
+function getNames(presidentsArr) {
+  const presNames = presidentsArr.map((pres) =>
+    pres.name
+  );
+  return presNames;
+}
+console.log(getNames(presidents));
 
 
 
